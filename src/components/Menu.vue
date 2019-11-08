291a12f36fe606a2c885b468ca1e04fd133214bd
JoseVictorLima <jsvctr123@gmail.com>
2019-11-08 16:56:40 -0300

    Adição do vue boostrap

---
+++ src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="menu-container row">
+            <div class="col-3">
+                <button class="btn btn-danger">Home</button>
+            </div>
+            <div class="col-3">
+                <button class="btn btn-danger">About</button>
+            </div>
+            <div class="col-3">
+                <button class="btn btn-danger">Login</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+.menu-container{
+    background-color: rgb(255, 195, 116)
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  BootstrapVue,
   render: h => h(App)
 }).$mount('#app')
