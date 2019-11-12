037aac05ed527f08d4e964444788ff81a0720714
David Sena <davidsenna.if@gmail.com>
2019-11-12 15:48:22 -0300

    perfil

---
+++ src/views/configPerfil.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="container">
+            <div class="row  justify-content-md-center">
+                <div class="col-6 a">
+                        <div class="media">
+                                <img class="align-self-start mr-3" src="imgtest.jpg" alt="Imagem de exemplo genérica" width="64px" height="64px">
+                                <div class="media-body">
+                                        aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>
+                                        eeeeeeeee <br>
+                                        aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>   
+                                        aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>   
+                                        aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>  
+                                        7aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>   
+                                        aaaaaaaaaaa <br>
+                                        bbbbbbbbb <br>
+                                        cccccccccc <br>
+                                        ddddddddddd <br>
+                                </div>
+                              </div>
+                </div>
+                
+            </div> 
+        </div>
+    
+       
+    
+
+</template>
+
+ <style>
+        body{
+            background-color: chartreuse;
+        }
+
+        .a{
+            background-color: cornflowerblue;
+            
+        }
+</style>
