caeba2f9c883b75ae5d5a9b9ea7d3ede653daaa7
David Sena <davidsenna.if@gmail.com>
2019-11-15 14:18:17 -0300

    Recuperar Senha

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,12 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('../views/Login.vue')
+  },
+
+  {
+    path: '/recuperarsenha',
+    name: 'recuperarsenha',
+    component: () => import('../views/recuperarSenha.vue')
   }
 ]
 
